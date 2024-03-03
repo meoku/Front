c5fab3ca7f650a5f2c1045deba22bc411d852ab1
wjdtj9656 <wjdtj9656@naver.com>
2024-03-03 21:50:12 +0900

    build: babel with typeError

--- meoku/vite.config.ts
+++ meoku/vite.config.ts
@@ -5,7 +5,8 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [
     react({
-      jsxImportSource: "@emotion/react",
+      jsxImportSource: "@emotion/ react",
+      // @ts-expect-error: Unreachable code error
       babel: {
         plugins: ["@emotion/babel-plugin"],
       },
