8eac218ea07874345a8cc34dbdea4cc9e1c93e9e
wjdtj9656 <wjdtj9656@naver.com>
2024-03-03 21:12:58 +0900

    build: project setup

---
+++ meoku/src/main.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NotFoundPage from "./pages/common/NotFoundPage.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFoundPage />,
+  },
+  // {
+  //   path: "/ex",
+  //   element: <ExForm />,
+  // },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
