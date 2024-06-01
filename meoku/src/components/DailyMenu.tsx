358956aaa7df120fb1ec561f9ed0f2b82115eacc
wjdtj9656 <wjdtj9656@naver.com>
2024-06-02 00:05:55 +0900

    design: change width in menu

--- meoku/src/components/DailyMenu.tsx
+++ meoku/src/components/DailyMenu.tsx
@@ -40,7 +40,7 @@ const DailyMenu = ({ dayWeek, day, menuData }: mainDailyMenuTime) => {
             flex-direction: column;
             justify-content: center;
             align-items: center;
-            width: 166px;
+            width: 242px;
             height: 200px;
             margin-top: 16px;
           `}
@@ -107,7 +107,7 @@ const DailyMenu = ({ dayWeek, day, menuData }: mainDailyMenuTime) => {
             flex-direction: column;
             justify-content: center;
             align-items: center;
-            width: 166px;
+            width: 242px;
             /* height: 170px; */
           `}
         >
@@ -124,10 +124,7 @@ const DailyMenu = ({ dayWeek, day, menuData }: mainDailyMenuTime) => {
             css={css`
               margin-top: 6px;
               text-align: center;
-              /* letter-spacing: ${menuData.detailedMenuDTOList[1].mainMenuName
-                .length > 4
-                ? "-3px"
-                : "0px"}; */
+              /* letter-spacing: -3px; */
             `}
           >
             {menuData.detailedMenuDTOList[1].menu1Name}
@@ -169,7 +166,7 @@ const DailyMenu = ({ dayWeek, day, menuData }: mainDailyMenuTime) => {
             flex-direction: column;
             justify-content: center;
             align-items: center;
-            width: 166px;
+            width: 242px;
             height: 150px;
             margin-top: 10px;
           `}
