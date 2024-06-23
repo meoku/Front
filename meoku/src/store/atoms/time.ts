477023d6142a790476fe4456dd9375ea1d0804ff
wjdtj9656 <wjdtj9656@naver.com>
2024-06-23 19:45:57 +0900

    fix: prev/next week error

--- meoku/src/store/atoms/time.ts
+++ meoku/src/store/atoms/time.ts
@@ -2,8 +2,7 @@ import { atom } from "recoil";
 
 const timeState = atom({
   key: "timeState",
-  default: new Date(new Date().setDate(new Date().getDate() - 3)),
-  // default: new Date(),
+  default: new Date(),
 });
 
 export default timeState;
