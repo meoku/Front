f19519eda704addabd48116de66944c6e7bcb846
wjdtj9656 <wjdtj9656@naver.com>
2025-02-22 21:29:36 +0900

    feat: refreshToken 적용

--- meoku/src/api/axiosConfig.ts
+++ meoku/src/api/axiosConfig.ts
@@ -9,6 +9,7 @@ const axiosInstance = axios.create({
 // 요청 인터셉터 추가
 axiosInstance.interceptors.request.use(
   (config) => {
+    window.location.href = "/login";
     // sessionStorage에서 accessToken 값을 가져옴
     const token = sessionStorage.getItem("access_token");
     if (token) {
@@ -18,6 +19,52 @@ axiosInstance.interceptors.request.use(
     return config;
   },
   (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// 401 에러 발생 시 refresh token으로 access token 갱신
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    // 만약 401 에러가 발생했고, 아직 재시도하지 않았다면(_retry 플래그로 체크)
+    if (
+      error.response &&
+      error.response.status === 401 &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
+      const refreshToken = sessionStorage.getItem("refresh_token");
+
+      if (refreshToken) {
+        try {
+          // refresh token을 사용해 새로운 access token 요청
+          const response = await axiosInstance.post(
+            "/auth/refreshAccessToken",
+            {
+              refreshToken,
+            }
+          );
+          const newAccessToken = response.data.access_token;
+          const newRefreshToken = response.data.refresh_token;
+          // 새로운 access token을 저장
+          sessionStorage.setItem("access_token", newAccessToken);
+          sessionStorage.setItem("refresh_token", newRefreshToken);
+          // 원래 요청의 헤더에 새로운 토큰 적용
+          originalRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
+          // 원래 요청 재시도
+          return axiosInstance(originalRequest);
+        } catch (refreshError) {
+          // refresh token이 만료되었거나 갱신에 실패
+          sessionStorage.removeItem("access_token");
+          sessionStorage.removeItem("refresh_token");
+          window.location.href = "/login";
+          return Promise.reject(refreshError);
+        }
+      }
+    }
     return Promise.reject(error);
   }
 );
