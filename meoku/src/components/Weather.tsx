8532bf73c1486aa3ed128d3c5984e43f414867bd
wjdtj9656 <wjdtj9656@naver.com>
2025-04-16 05:50:09 +0900

    refact : 날씨 함수 리팩토링

--- meoku/src/components/Weather.tsx
+++ meoku/src/components/Weather.tsx
@@ -1,36 +1,9 @@
 import { css } from "@emotion/react";
 // import snowImage from "/weather/ImageSnow.svg";
-import icCloudy from "/weather/cloudy.svg";
-import icSun from "/weather/sunny.svg";
-import icDark from "/weather/dark.svg";
-import icRain from "/weather/rain.svg";
-import icSnow from "/weather/snow.svg";
-import icSnowRain from "/weather/snowRain.svg";
-import icHeavyRain from "/weather/snowRain.svg";
 import { useQuery } from "@tanstack/react-query";
 import { fetchWeatherData } from "../api/weatherApi";
+import { getWeatherImg } from "../utils/weatherUtils";
 
-const getWeatherImg = (isRain: boolean, value: string) => {
-  if (isRain) {
-    if (value == "1") {
-      return icRain;
-    } else if (value == "2") {
-      return icSnowRain;
-    } else if (value == "3") {
-      return icSnow;
-    } else if (value == "4") {
-      return icHeavyRain;
-    }
-  } else {
-    if (value == "1") {
-      return icSun;
-    } else if (value == "3") {
-      return icCloudy;
-    } else if (value == "4") {
-      return icDark;
-    }
-  }
-};
 const Weather = () => {
   const skyMap = new Map();
   const rainMap = new Map();
--- meoku/src/components/mobile/MobileApp.tsx
+++ meoku/src/components/mobile/MobileApp.tsx
@@ -23,13 +23,7 @@ import { useQuery } from "@tanstack/react-query";
 import { fetchMenuData } from "../../api/menuApi";
 import { defaultMenuData } from "../../utils/defaultMenuData";
 import { fetchWeatherData } from "../../api/weatherApi";
-import icCloudy from "/weather/cloudy.svg";
-import icSun from "/weather/sunny.svg";
-import icDark from "/weather/dark.svg";
-import icRain from "/weather/rain.svg";
-import icSnow from "/weather/snow.svg";
-import icSnowRain from "/weather/snowRain.svg";
-import icHeavyRain from "/weather/snowRain.svg";
+import { getWeatherImg } from "../../utils/weatherUtils";
 
 const MobileMain = styled.div`
   display: flex;
@@ -134,27 +128,6 @@ const MenuItem = styled.div`
     font-size: 18px;
   }
 `;
-const getWeatherImg = (isRain: boolean, value: string) => {
-  if (isRain) {
-    if (value == "1") {
-      return icRain;
-    } else if (value == "2") {
-      return icSnowRain;
-    } else if (value == "3") {
-      return icSnow;
-    } else if (value == "4") {
-      return icHeavyRain;
-    }
-  } else {
-    if (value == "1") {
-      return icSun;
-    } else if (value == "3") {
-      return icCloudy;
-    } else if (value == "4") {
-      return icDark;
-    }
-  }
-};
 interface RequestData {
   date: string;
 }
